a003537e1b3009f4f444644792bcaacc40748b36
CMDR-WDX <83889860+CMDR-WDX@users.noreply.github.com>
2022-07-28 13:35:43 +0200

    initial commit

---
+++ load.py
@@ -0,0 +1,32 @@
+import tkinter
+import requests
+
+_plugin_name = "eliteheatmap"
+
+SERVER_PORT = 8000
+
+
+def plugin_app(parent: tkinter.Frame) -> tkinter.Frame:
+    return parent
+
+
+def plugin_start3(_path: str) -> str:
+    return _plugin_name
+
+
+def journal_entry(_cmdr: str, _beta: bool, _system: str, _station: str, entry: dict[str, any], _state: dict):
+    # Only cares about FSD Jumps
+    if entry["event"] != "FSDJump":
+        return
+
+    sysname = entry["StarSystem"]
+    [x, y, z] = entry["StarPos"]
+
+    jsonData = dict([
+        ("systemName", sysname),
+        ("x", x),
+        ("y", y),
+        ("z", z),
+    ])
+
+    requests.post("http://localhost:"+str(SERVER_PORT), json=jsonData)
